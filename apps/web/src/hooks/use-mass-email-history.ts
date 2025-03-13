011eba7c2df4aa5bf142b9bd045d5ebd73010c54
Sunghwan Park <sunghwan.park@ca.ey.com>
2025-03-13 14:18:04 -0700

    fix mass-email-history url

--- apps/web/src/hooks/use-mass-email-history.ts
+++ apps/web/src/hooks/use-mass-email-history.ts
@@ -4,7 +4,7 @@ import { MassEmailHistory, PaginationQuery } from '@ehpr/common';
 
 export const useMassEmailHistory = ({ skip, limit }: PaginationQuery) => {
   const { data, ...response } = useSWR<{ data: MassEmailHistory }>(
-    `/registrants/mass-email?skip=${skip}&limit=${limit}`,
+    `/registrants/mass-email-history?skip=${skip}&limit=${limit}`,
     axios.get,
   );
 
