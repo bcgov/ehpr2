b0c5b230a5bab6fc520dbfa8322645ea4c7c3cca
Chloe Yu <c.yu@cgi.com>
2026-04-28 13:10:38 -0700

    set width and height attributes for the moh logo directly

--- apps/api/src/mail/mail.util.ts
+++ apps/api/src/mail/mail.util.ts
@@ -3,7 +3,7 @@ export const getBodyWithFooter = (body: string, token: string, domain?: string) 
     ? `https://${domain}/unsubscribe?token=${token}`
     : `https://ehpr.gov.bc.ca/unsubscribe?token=${token}`;
 
-  const logoUrl = domain
+  const logoUrl = domain && !domain.startsWith('localhost')
     ? `https://${domain}/assets/img/MOH_Logo.png`
     : `https://ehpr.gov.bc.ca/assets/img/MOH_Logo.png`;
 
@@ -18,7 +18,9 @@ export const getBodyWithFooter = (body: string, token: string, domain?: string) 
                 <img
                   src="${logoUrl}"
                   alt="Ministry of Health"
-                  style="width: 60px; height: auto;"
+                  width="100"
+                  height="100"
+                  style="width: 100px; height: 100px;"
                 />
               </td>
               <td
